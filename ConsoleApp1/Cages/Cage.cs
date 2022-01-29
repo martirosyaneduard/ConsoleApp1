fd9be41d6497e060a6824dd0417fe7fe38f5f49e
Eduard Martirosyan <martirosyane10@gmail.com>
2022-01-29 20:25:44 +0400

    Cancge animal and cage

--- ConsoleApp1/Cages/Cage.cs
+++ ConsoleApp1/Cages/Cage.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ConsoleApp1.Animals;
+using ConsoleApp1.Foods;
 
 namespace ConsoleApp1.Cages
 {
-    abstract class Cage
+    public class Cage : ICage
     {
-        public int Length { get;protected set; }
+        public int Length { get; protected set; }
         public int Width { get; protected set; }
         public int Height { get; protected set; }
-        public bool OpenOrCloseDoor { get;protected set; } = false;
-        public void Door()
+        private List<Food> _foods { get; set; } = new List<Food>();
+        public List<Food> Foods => _foods;
+        public List<Animal> Animals { get; set; } = new List<Animal>();
+        public event Action FoodArived;
+
+        public void AddAnimal(Animal animal)
         {
-            if (!OpenOrCloseDoor)
-            {
-                Console.WriteLine("Vandaki dure pak e");
-            }
-            else
-            {
-                Console.WriteLine("Vandaki dure bac e");
-            }
+            Animals.Add(animal);
+            animal.SetCage(this);
+        }
+
+        public void RemoveFood(Food food)
+        {
+            _foods.Remove(food);
+        }
+
+        public void PutFood(Food food)
+        {
+            _foods.Add(food);
+            FoodArived?.Invoke();
         }
 
 
